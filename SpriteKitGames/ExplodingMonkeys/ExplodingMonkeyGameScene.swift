af194accb53d430df0c54fa32cc13c0995cfbd51
Ashraful Islam <ashraful.meche@gmail.com>
2025-03-23 01:42:24 -0500

    add the players for exploding monkey

--- SpriteKitGames/ExplodingMonkeys/ExplodingMonkeyGameScene.swift
+++ SpriteKitGames/ExplodingMonkeys/ExplodingMonkeyGameScene.swift
@@ -19,9 +19,14 @@ class ExplodingMonkeyGameScene: SKScene {
     var isPlayerDisabled: Binding<Bool>?
     var activePlayer: Binding<monkeyPlayer>?
     
+    var player1: SKSpriteNode!
+    var player2: SKSpriteNode!
+    var banana: SKSpriteNode!
+    
     override func didMove(to view: SKView) {
         backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
         createBuildings()
+        createPlayers()
     }
     
     func createBuildings() {
@@ -41,6 +46,40 @@ class ExplodingMonkeyGameScene: SKScene {
 
     }
     
+    func createPlayers() {
+        player1 = SKSpriteNode(imageNamed: "monkeyPlayer")
+        player1.name = "player1"
+        player1.physicsBody = SKPhysicsBody(circleOfRadius: player1.size.width / 2)
+        player1.physicsBody?.categoryBitMask = eMonkeyCollisionType.player.rawValue
+        player1.physicsBody?.collisionBitMask = eMonkeyCollisionType.banana.rawValue
+        player1.physicsBody?.contactTestBitMask = eMonkeyCollisionType.banana.rawValue
+        player1.physicsBody?.isDynamic = false
+        
+        let player1Building = buildings[1]
+        player1.position = CGPoint(
+            x: player1Building.position.x,
+            y: player1Building.position.y + (player1Building.size.height + player1.size.height) / 2
+        )
+        
+        addChild(player1)
+        
+        player2 = SKSpriteNode(imageNamed: "monkeyPlayer")
+        player2.name = "player2"
+        player2.physicsBody = SKPhysicsBody(circleOfRadius: player2.size.width / 2)
+        player2.physicsBody?.categoryBitMask = eMonkeyCollisionType.player.rawValue
+        player2.physicsBody?.collisionBitMask = eMonkeyCollisionType.banana.rawValue
+        player2.physicsBody?.contactTestBitMask = eMonkeyCollisionType.banana.rawValue
+        player2.physicsBody?.isDynamic = false
+        
+        let player2Building = buildings[buildings.count - 2]
+        player2.position = CGPoint(
+            x: player2Building.position.x,
+            y: player2Building.position.y + (player2Building.size.height + player2.size.height) / 2
+        )
+        
+        addChild(player2)
+    }
+    
     func launch(angle: Int, velocity: Int) {
         isPlayerDisabled?.wrappedValue = true
         
@@ -49,5 +88,7 @@ class ExplodingMonkeyGameScene: SKScene {
             self.activePlayer?.wrappedValue = self.activePlayer?.wrappedValue == .player1 ? .player2 : .player1
         }
     }
+    
+    
 
 }
